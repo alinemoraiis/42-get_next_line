2fae29b62734b7ef1beb9eb6deac09d1a77f5e96
Aline Morais <moraisraline@gmail.com>
2022-03-23 11:33:33 +0000

    get_next_line

---
+++ get_next_line.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ados-rei <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/17 21:26:23 by ados-rei          #+#    #+#             */
+/*   Updated: 2022/03/22 13:35:16 by ados-rei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+
+char	*read_file(int fd, char *file);
+char	*get_line(char *file);
+char	*save_file(char *file);
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+
+#endif
